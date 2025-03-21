b59ff7d45897935e1126625adafb44b453c89932
Damesow2003 <sowgokuuza@gmail.com>
2025-03-21 06:13:09 +0000

    add dashbord medecine

---
+++ front-end/src/app/dashbord/components/medecine-dashbord/medecine-dashbord.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medecine-dashbord',
+  templateUrl: './medecine-dashbord.component.html',
+  styleUrl: './medecine-dashbord.component.scss'
+})
+export class MedecineDashbordComponent {
+
+}
